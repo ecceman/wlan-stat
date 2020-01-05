10525fea9d61e6a6138d50717634f66fb7ee8ab7
ecceman <39990046+ecceman@users.noreply.github.com>
2020-01-05 13:12:08 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # wlan-stat
-Polybar WLAN status module
+Polybar WLAN status module that shows more details. Useful for WLAN troubleshooting, probably most interesting for network technicians. Shows
+- SSID name
+- Signal strength in dBm
+- Channel
+- Channel bandwidth
+- AP Mac address that is resolved to AP name if found in ap-list.csv file
+
+Since SSID addresses are usually generated only the first 11 characters are matched when resolving the AP name.
---
+++ wlan-stat.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import subprocess
+import re
+from pathlib import Path
+
+wnic = "wlp61s0"
+
+cmd_ssid = subprocess.Popen(['nmcli | grep "connected to" | awk \'{print $4}\''], stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT, shell=True)
+ssid = cmd_ssid.stdout.read().decode('utf-8').strip('\n')
+
+cmd_ap = subprocess.Popen(['nmcli -f BSSID,ACTIVE dev wifi list | awk \'$2 ~ /yes/ {print $1}\''],
+                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+ap = cmd_ap.stdout.read().decode('utf-8').strip('\n').lower()
+
+cmd_rssi = subprocess.Popen(['cat /proc/net/wireless | grep ' + wnic + ' | cut -d " " -f 7 | head -c 3'],
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+rssi = cmd_rssi.stdout.read().decode('utf-8')
+rssi = rssi.strip('\n')
+
+cmd_ch = subprocess.Popen(
+    ['sudo iwlist ' + wnic + ' frequency |  grep Current | awk \'{print $5}\' | sed s/[^0-9]*//g'],
+    stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+ch = cmd_ch.stdout.read().decode('utf-8')
+ch = ch.strip('\n')
+
+cmd_ht = subprocess.Popen(['sudo iw dev | grep -i ' + wnic + ' -A 10 | grep -w "width: [0-9]*" | awk \'{print $6}\''],
+                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+ht = cmd_ht.stdout.read().decode('utf-8')
+ht = ht.strip('\n')
+
+# Find name of AP
+home = str(Path.home())
+try:
+    f = open(home + "/.config/polybar/ap_list.csv", "r")
+    data = f.readlines()
+    for line in data:
+        line = line.strip('\n').lower()
+        ap_name, mac = line.split(",")
+
+        if ap[:16] == mac[:16]:
+            ap = ap_name
+            break
+except IOError:
+    pass
+
+print(ssid + ", " + rssi + " dBm, Ch " + ch + ", HT" + ht + " @ " + ap)
+
